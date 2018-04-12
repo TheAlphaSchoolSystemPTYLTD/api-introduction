9c683a8b76dc2f188ca050d9d1df909c5c77250d
Ricardo Russon <ricardo.russon@tassweb.com.au>
2018-04-12 10:01:22 +1000

    Updated formatting

--- encryptDecrypt.cs
+++ encryptDecrypt.cs
@@ -10,7 +10,7 @@ public class TestEncrypt
 	const String companyCode = "10";
 	const String version = "2";
 	const String method = "getStudents";
-	const String endPoint = "http://tass.school.edu.au/tassweb/api/";
+	const String endPoint = "http://api.tasscloud.com.au/tassweb/api/";
 	
 	public static void Main()
 	{
@@ -26,49 +26,49 @@ public class TestEncrypt
 		
 	}
 
- 	public static string Encrypt (string toEncrypt)
-    {
-        var UTF8 = new System.Text.UTF8Encoding();
+	public static string Encrypt (string toEncrypt)
+	{
+		var UTF8 = new System.Text.UTF8Encoding();
 		
 		// The Token Key is already Base64.
 		byte[] keyArray   	  = Convert.FromBase64String(tokenKey);
 		
 		// The Paramater String is UTF8.
-        byte[] toEncryptArray = UTF8.GetBytes(toEncrypt);
+		byte[] toEncryptArray = UTF8.GetBytes(toEncrypt);
 
-        RijndaelManaged rDel  = new RijndaelManaged();
-        rDel.Key              = keyArray;
-        rDel.Mode             = CipherMode.ECB;
-        rDel.BlockSize        = 128;
-        rDel.Padding   		  = PaddingMode.PKCS7;
+		RijndaelManaged rDel  = new RijndaelManaged();
+		rDel.Key              = keyArray;
+		rDel.Mode             = CipherMode.ECB;
+		rDel.BlockSize        = 128;
+		rDel.Padding   		  = PaddingMode.PKCS7;
 
-        ICryptoTransform cTransform  = rDel.CreateEncryptor( keyArray , null );
+		ICryptoTransform cTransform  = rDel.CreateEncryptor( keyArray , null );
 
-        byte[] resultArray    = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+		byte[] resultArray    = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
 		
 		return Convert.ToBase64String(resultArray, 0, resultArray.Length);
-    }
+	}
 
-    public static string Decrypt (string toDecrypt)
-    {
-        var UTF8 = new System.Text.UTF8Encoding();
+	public static string Decrypt (string toDecrypt)
+	{
+		var UTF8 = new System.Text.UTF8Encoding();
 		
 		// The Token Key is already Base64.
 		byte[] keyArray  	  = Convert.FromBase64String(tokenKey);
 		
 		// The encrypted string is already Base64.
-        byte[] toEncryptArray = Convert.FromBase64String(toDecrypt);
+		byte[] toEncryptArray = Convert.FromBase64String(toDecrypt);
 
-        RijndaelManaged rDel  = new RijndaelManaged();
-        rDel.Key              = keyArray;
-        rDel.Mode             = CipherMode.ECB;
-        rDel.BlockSize        = 128;
-        rDel.Padding    	  = PaddingMode.PKCS7;
+		RijndaelManaged rDel  = new RijndaelManaged();
+		rDel.Key              = keyArray;
+		rDel.Mode             = CipherMode.ECB;
+		rDel.BlockSize        = 128;
+		rDel.Padding    	  = PaddingMode.PKCS7;
 	
-        ICryptoTransform cTransform = rDel.CreateDecryptor( keyArray , null );
+		ICryptoTransform cTransform = rDel.CreateDecryptor( keyArray , null );
 
-        byte[] resultArray    = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+		byte[] resultArray    = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
 
-        return UTF8.GetString(resultArray);
-    }	
+		return UTF8.GetString(resultArray);
+	}	
 }
