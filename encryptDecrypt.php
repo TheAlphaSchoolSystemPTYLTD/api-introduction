211943022baaf665bb8bb97971fa418900699561
Joshua Todd Cowper <joshua@cowper.id.au>
2019-03-02 20:53:20 +1000

    Update PHP encryptDecrypt example to use OpenSSL
    * As Mcrypt is now deprecated, update code to use OpenSSL for token
    encryption and decryption (fixes #7)
    * Refactor other code in the example

--- encryptDecrypt.php
+++ encryptDecrypt.php
@@ -1,66 +1,46 @@
 <?php
-/*Author: Liam Stevens (ICT Analyst, St Joseph's College Gregory Terrace)
-Created on: 21/8/2018
+// An example of encrypting and decrypting your Token and accessing the API using PHP; specifically the getStudentsDetails method.
 
-Based on a C# example by @ricardorusson and PS example by @sam-fisher:
-https://github.com/TheAlphaSchoolSystemPTYLTD/api-introduction/blob/master/encryptDecrypt.cs
-https://github.com/TheAlphaSchoolSystemPTYLTD/api-introduction/blob/master/EncryptDecrypt.ps1
-
-An example of encrypting and decrypting your Token and accessing the API using PHP; specifically the getStudentsDetails method.*/
-
-//Constants to use when generating API requests - change these as per your TASS API Gateway
+	// Constants to use when generating API requests - change these as per your TASS API Gateway
 	$tokenKey = 'x8FWQUedjyiUGlTf5appPQ==';
 	$appCode = 'DEMOAPP';
 	$companyCode = '10';
 	$apiVersion = '2';
-	$method = 'GetStudentsDetails';
-	$endPoint = 'http://api.tasscloud.com.au/tassweb/api/';
-	$parameters = "{\"currentstatus\":\"current\"}";
+	$method = 'getStudentsDetails';
+	$endPoint = 'https://api.tasscloud.com.au/tassweb/api/';
+	$parameters =  '{"currentstatus":"current"}';
 
-	function Decrypt($s){
-		global $tokenKey;
-
-		if ($s == "") { return $s; }
-		// Turn the cipherText into a ByteArray from Base64
-		try {
+	function Encrypt($tokenKey, $parameters) {
 		$decodedKey = base64_decode($tokenKey);
-		$s = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $decodedKey, $s, MCRYPT_MODE_ECB);
-		} catch(Exception $e) {
-		// There is a problem with the string, perhaps it has bad base64 padding
-		// Do Nothing
-		}
-		return $s;
-	}
-
-	function Encrypt($s){
-		global $tokenKey;
-
-		$decodedKey = base64_decode($tokenKey);
-		// Have to pad if it is too small
-		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB);
-		$pad = $block - (strlen($s) % $block);
-		$s .= str_repeat(chr($pad), $pad);
-		$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $decodedKey, $s, MCRYPT_MODE_ECB);
+		$encrypted = openssl_encrypt($parameters, 'aes-128-ecb', $decodedKey, OPENSSL_RAW_DATA);
 		$encoded = base64_encode($encrypted);
 		return $encoded;
 	}
 
+	function Decrypt($tokenKey, $s) {
+		$decodedKey = base64_decode($tokenKey);
+		$s = base64_decode($s);
+		$s = openssl_decrypt($s, 'aes-128-ecb', $decodedKey, OPENSSL_RAW_DATA);
+		return $s;
+	}
+
 	function getURLRequest($endPoint, $method, $appCode, $companyCode, $apiVersion, $parameters, $tokenKey) {
-		$encryptedkey = Encrypt($parameters);
-		$dict = array('method'=>$method, 'appcode'=>$appCode, 'company'=>$companyCode, 'v'=>$apiVersion, 'token'=>$encryptedkey);
-		$keys = array_keys($dict);
+		$encryptedToken = Encrypt($tokenKey, $parameters);
+		$dict = array('method' => $method, 'appcode' => $appCode, 'company' => $companyCode, 'v' => $apiVersion, 'token' => $encryptedToken);
 		$URLString = $endPoint . '?';
-		foreach($keys as $value) {
-			$URLString .= $value;
+		foreach($dict as $key => $value) {
+			$URLString .= $key;
 			$URLString .= '=';
-			$URLString .= urlencode($dict[$value]);
+			$URLString .= urlencode($value);
 			$URLString .= '&';
 		}
 		// Trim the last ampersand because it's not necessary
 		$URLString = substr($URLString, 0, -1);
-		#Echo for example purposes, remove this in production
-		echo $URLString;
 		return $URLString;
 	}
-
+	
+	echo 'Original:   ', $parameters, PHP_EOL;
+	echo 'Encrypted:  ', Encrypt($tokenKey, $parameters), PHP_EOL;
+	echo 'Decrypted:  ', Decrypt($tokenKey, Encrypt($tokenKey, $parameters)), PHP_EOL;
+	echo 'URL:        ', getURLRequest($endPoint, $method, $appCode, $companyCode, $apiVersion, $parameters, $tokenKey), PHP_EOL;
 ?>
